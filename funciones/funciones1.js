cc3aa92cb240c55c85e15e7ca46048fe6fd00725
alejo577 <alejandropulido577@gmail.com>
2022-04-29 16:46:56 -0500

    programas js con fecha

--- funciones/funciones1.js
+++ funciones/funciones1.js
@@ -108,8 +108,21 @@ function descuento (valor,pordes){
 
 descuento(100000,15)
 
+//FECHA
 
-
-
-
-
+function fecha(a) {
+var dia = new Date(a);
+var hoy = new Date();
+console.log(dia);
+console.log(hoy);
+console.log()
+var x = hoy.toISOString().split("T")[0]
+if(dia<x){
+    console.log("la fecha que coloco es anterior")
+}else if(dia>x){
+    console.log("la fecha que coloco es posterior")
+}else{
+    console.log("la fecha que coloco es actual")
+}
+}
+fecha("2022-04-29")
